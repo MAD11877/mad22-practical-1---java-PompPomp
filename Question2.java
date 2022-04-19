51b42e74a17f99962343a54b1bbaeec2aa24935a
Wee Kang Lee <s10222162@connect.np.edu.sg>
2022-04-19 19:32:43 +0800

    Q2

--- Question2.java
+++ Question2.java
@@ -11,6 +11,17 @@ public class Question2
      */
      
     Scanner in = new Scanner(System.in);
+
+
+    System.out.println("Enter your height: ");
     double height = in.nextDouble();
+
+    System.out.println("Enter your weight: ");
+    double weight = in.nextDouble();
+
+    double bmi = weight / (height * height);
+
+    System.out.println(bmi);
+
   }
 }
